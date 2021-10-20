208629c90220433e5930346f4675f9e3fc01e26a
adavila <adavila@optimeconsulting.com>
2021-10-20 12:36:54 -0500

    Fetch API comics with Vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="App" v-if="!loading && data">
+    <div class="App-header">
+      <h1>{{data.title}}</h1>
+      <time>{{`${data.day}/${data.month}/${data.year}`}}</time>
+      <img :src="data.img" :alt=data.alt >
+      <p>{{data.alt}}</p>
+      <div v-if="loading">
+        Cargando...
+      </div>
+      <div v-if="errored">
+        Error {{error}}
+      </div>
+      <Rating />
+    </div>
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import axios from "axios"
+  import Rating from "./components/Rating.vue"
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data(){
+      return {
+        data: null,
+        error: null,
+        loading: true,
+        errored: false
+      }
+    },
+    mounted() {
+      axios
+        .get('https://cors-anywhere.herokuapp.com/https://xkcd.com/info.0.json')
+        .then(response => {
+          this.data = response.data
+        })
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+          this.error = error.message
+        })
+        .finally(() => this.loading = false)
+    },
+    components: {
+      Rating
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  .App {
+    text-align: center;
+    &-logo {
+      height: 40vmin;
+      pointer-events: none;
+    }
+    &-header {
+      background-color: #282c34;
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      font-size: calc(10px + 2vmin);
+      color: white;
+    }
+    &-link {
+      color: #61dafb;
+    }
+  }
 </style>
---
+++ src/components/Rating.vue
@@ -0,0 +1,45 @@
+<template>
+  <div className="rating-container">
+      <label className="rating-container__star" v-for="(item, key) in [...Array(5)]" :key="key">
+        <input type="radio" v-model="rate" :value="key+1" />
+        <font-awesome-icon :icon="appIcon" style="color: rgb(192,192,192)" v-bind:class="[(key+1 < rate || key+1 === rate) ? activeClass : '' ]" />
+      </label >
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon  } from '@fortawesome/vue-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+export default {
+  name: 'Rating',
+  data (){
+    return {
+      rate: 0,
+      activeClass: "active"
+    }
+  },
+  components: {
+    FontAwesomeIcon
+  },
+  computed: {
+    appIcon () {
+      return faStar
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.rating-container {
+  &__star{
+    input[type=radio]{
+      display: none;
+    }
+    .active {
+      color: #000 !important
+    }
+  }
+}
+</style>
